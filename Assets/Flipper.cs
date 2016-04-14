eff9e75f4fe8915ee9a0307e242320790ac3ed13
Teun <teun.willems@live.nl>
2016-04-14 15:02:03 +0200

    Added random rocks

---
+++ Assets/Flipper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Flipper : MonoBehaviour
+{
+
+    public static System.Random Random;
+
+    void Start ()
+    {
+        if (Random == null) Random = new System.Random();
+        GetComponent<SpriteRenderer>().flipX = Random.NextDouble() > .5;
+    }
+}
