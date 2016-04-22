a319c7bdde5ef2341eb619096bf51c63e5f3843a
Teun <teun.willems@live.nl>
2016-04-22 10:01:08 +0200

    Commit files for some reason

--- Assets/_Scripts/Manager/UIManager.cs
+++ Assets/_Scripts/Manager/UIManager.cs
@@ -10,10 +10,23 @@ namespace Assets._Scripts
         public int MovesMade;
 
         public Text MoveText;
+        public InputField nameInputField;
 
-        // Update is called once per frame
+        void Start()
+        {
+            if (nameInputField != null)
+            {
+                nameInputField.text = PlayerPrefs.GetString(Highscore.UsernameKey);
+            }
+        }
+
+        // Update is called once per framed
         void Update()
         {
+            if (nameInputField != null)
+            {
+                PlayerPrefs.SetString(Highscore.UsernameKey, nameInputField.text);
+            }
             MoveText.text = "Moves made: " + MovesMade;
         }
     }
